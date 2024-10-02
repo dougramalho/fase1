b8d5681f25c9bf110d6433718af8c553b5956a5c
Douglas José Ramalho Araujo <douglas.ramalho@squadra.com.br>
2024-10-01 21:36:10 -0300

    alterando venv

--- pos/dev/Scripts/activate.bat
+++ pos/dev/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\pessoal\projetos\pos\dev
+set VIRTUAL_ENV=D:\pessoal\projetos\fase1\pos\dev
 
 if not defined PROMPT set PROMPT=$P$G
 
